32822bf095e2e5302c0b087e912977a794b84943
Madhubhashana Mahindarathne <104136158+Madhubhashana99@users.noreply.github.com>
2024-04-09 20:32:29 +0530

    update

--- word_counter/src/WordCounter.java
+++ word_counter/src/WordCounter.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -14,6 +15,21 @@ public class WordCounter {
     }
 
     public static Map<String,Integer> countWords(String sentence){
-        
+        Map<String,Integer> wordCountMap = new HashMap<>();
+        String[] words= sentence.split("\\s+");
+
+        for (String word: words){
+            word = word.replaceAll("[^a-zA-Z]","");
+            word = word.toLowerCase();
+            if (!word.isEmpty()) {
+                if (wordCountMap.containsKey(word)) {
+                    int count = wordCountMap.get(word);
+                    wordCountMap.put(word, count + 1);
+                } else {
+                    wordCountMap.put(word, 1);
+                }
+            }
+        }
+        return wordCountMap;
     }
 }
